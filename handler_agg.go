7d0c823a07cab52c02374cc5f6fa02ed30ae711a
CoDanTheBarbarian <plafcandaniel@gmail.com>
2024-10-03 14:57:53 -0700

    12: Updated the handlerAggregate to run a feed scraper in an endless loop on an interval based on the given duration string argument. Created a 4th migration to add a feed fetched timestamp to the feeds. Created scrapeFeed/s functions to fetch feed RSS data and print the titles of the RSS items in the fetched feed.

--- handler_agg.go
+++ handler_agg.go
@@ -3,13 +3,47 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"time"
+
+	"github.com/CoDanTheBarbarian/gator/internal/database"
 )
 
 func handlerAggregate(s *state, cmd command) error {
-	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
-	if err != nil {
-		return err
+	if len(cmd.input) < 1 {
+		return fmt.Errorf("usage: agg <duration_string>")
 	}
-	fmt.Printf("%+v\n", feed)
-	return nil
+	timeBetweenRequests, err := time.ParseDuration(cmd.input[0])
+	if err != nil {
+		return fmt.Errorf("input is not a duration string: %v", err)
+	}
+	fmt.Printf("Collecting feeds every %s", cmd.input[0])
+	ticker := time.NewTicker(timeBetweenRequests)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+}
+
+func scrapeFeeds(s *state) {
+	feed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		log.Printf("Couldn't get next feed to fetch: %v", err)
+	}
+	log.Printf("Found a feed to fetch!")
+	scrapeFeed(s.db, feed)
+}
+
+func scrapeFeed(db *database.Queries, feed database.Feed) {
+	_, err := db.MarkFeedFetched(context.Background(), feed.ID)
+	if err != nil {
+		log.Printf("Couldn't mark feed %s fetched: %v", feed.Name, err)
+	}
+	feedData, err := fetchFeed(context.Background(), feed.Url)
+	if err != nil {
+		log.Printf("Couldn't fetch feed %s: %v", feed.Name, err)
+	}
+	for _, item := range feedData.Channel.Item {
+		fmt.Printf("Found post: %s\n", item.Title)
+	}
+	log.Printf("Feed %s collected, %v posts found", feed.Name, len(feedData.Channel.Item))
 }
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -15,3 +15,17 @@ SELECT * FROM feeds;
 
 -- name: GetFeedFromUrl :one
 SELECT * FROM feeds WHERE feeds.url = $1;
+
+-- name: MarkFeedFetched :one
+-- Requires migration 004: Add last_fetched_at column to feeds table
+UPDATE feeds
+SET last_fetched_at = NOW(), updated_at = NOW()
+WHERE id = $1
+RETURNING *;
+
+-- name: GetNextFeedToFetch :one
+-- Requires migration 004: Add last_fetched_at column to feeds table
+SELECT *
+FROM feeds
+ORDER BY last_fetched_at NULLS FIRST
+LIMIT 1;
